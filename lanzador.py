082c167a23876a7bd49ddac409a8fbbb900b7340
jzazooro <91785177+jzazooro@users.noreply.github.com>
2024-02-19 19:06:58 +0100

    lanzador

---
+++ lanzador.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import subprocess
+
+def main():
+    visual = int(input("¿Que datos desea visualizar?"
+                   "1. EQUIPOS QUE MAS PUNTOS OBTIENEN EN FASE DE GRUPOS"
+                   "2. EQUIPOS QUE MAS GOLES MARCAN EN FASE DE GRUPOS"
+                   "3. EQUIPOS QUE MAS GOLES RECIBEN EN FASE DE GRUPOS"
+                   "4. EQUIPOS QUE MAS GOLES MARCAN DE LOCAL"
+                   "5. EQUIPOS QUE MAS GOLES MARCAN DE VISITANTE"
+                   "6. PUNTOS OBTENIDOS DE LOCAL"
+                   "7. PUNTOS OBTENIDOS DE VISITANTE"
+                   "8. ANALISIS BARCELONA FASE DE GRUPOS"
+                   "9. DIAGRAMA DE GOLES MARCADOS EN CASA VS VISITANTE EN FASE DE GRUPOS"
+                   "10. PARTIDOS CON GOLES DE AMBOS EQUIPOS VS. PARTIDOS SIN GOLES DE AMBOS EQUIPOS EN TODA LA COMPETICIÓN"
+                   "11. MEDIA DE GOLES EN LOS PARTIDOS DE LA COMPETICION Y CORRELACION ENTRE GOLES MARCADOS Y PUNTOS OBTENIDOS"
+                   ))
+    if visual == "1":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis1.1.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+    
+    if visual == "2":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis1.2.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+
+    if visual == "3":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis1.3.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+
+    if visual == "4":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis1.4.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+
+    if visual == "5":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis1.5.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+
+    if visual == "6":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis1.6.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+
+    if visual == "7":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis1.7.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+
+    if visual == "8":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis2.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+
+    if visual == "9":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis3.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+
+    if visual == "10":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ANALISIS/analisis4.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
+
+    if visual == "11":
+        # Ruta al script que quieres ejecutar
+        script_path = 'ESTADISTICAS/estadisticas.py'
+        # Ejecutar el script
+        subprocess.run(['python', script_path], check=True)
---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import subprocess
+from lanzador import main
+
+if __name__ == "__main__":
+    main()
