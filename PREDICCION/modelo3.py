d3ecc33543d5d8e2f1e34f1c1bf6f6ed0e390723
jzazooro <91785177+jzazooro@users.noreply.github.com>
2024-05-13 11:58:18 +0200

    MODELOS TERMINADOS

--- PREDICCION/modelo3.py
+++ PREDICCION/modelo3.py
@@ -2,9 +2,34 @@
 
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+import csv
 
-# Cargar los datos
-data = pd.read_csv('DATOS/actualidad.csv')
+file_path = r'DATOS/actualidad.csv'
+data = pd.read_csv(file_path)
+
+data.head()
+
+# Limpiar los nombres de las columnas (eliminar espacios adicionales)
+data.columns = data.columns.str.strip()
+
+equipos = []
+
+with open(file_path, 'r', newline='') as archivo:
+    lector_csv = csv.reader(archivo)
+    for fila in lector_csv:
+        if fila:  # Verificar si la fila no está vacía
+            primer_elemento = fila[0]
+            equipos.append(primer_elemento)
+
+# pide al usuario que introduzca los equipos
+while True:
+    equipo1 = input("Introduce las siglas del primer equipo : ")
+    equipo2 = input("Introduce las siglas del segundo equipo: ")
+    
+    if equipo1 not in equipos or equipo2 not in equipos:
+        print("Al menos uno de los equipos no está en el archivo CSV. Por favor, inténtalo de nuevo.")
+    else:
+        break
 
 # Corregir los nombres de las columnas para quitar espacios al principio
 data.columns = data.columns.str.strip()
@@ -18,11 +43,11 @@ X = data[features]
 y = data['goles_promedio_por_partido']
 
 # Excluir "BAR" y "MAD" para el conjunto de entrenamiento y usarlos solo para pruebas
-train_indices = data[~data['equipo'].isin(['BAR', 'MAD'])].index
+train_indices = data[~data['equipo'].isin([equipo1, equipo2])].index
 X_train = X.loc[train_indices]
 y_train = y[train_indices]
 
-test_indices = data[data['equipo'].isin(['BAR', 'MAD'])].index
+test_indices = data[data['equipo'].isin([equipo1, equipo2])].index
 X_test = X.loc[test_indices]
 y_test = y[test_indices]
 
@@ -32,4 +57,8 @@ rf_model.fit(X_train, y_train)
 
 # Predecir los goles para "BAR" y "MAD"
 rf_predicted_goals = rf_model.predict(X_test)
-print(rf_predicted_goals)
+rf_predicted_goals
+
+resultadopredicho =[round(num) for num in rf_predicted_goals]
+
+print(equipo1, resultadopredicho[0], " - ", equipo2, resultadopredicho[1])
